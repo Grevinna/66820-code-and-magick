301017336eced81b6bfdf7597550a03c52f22c5d
Olesya Shtykovskaya <olesya.sht@gmail.com>
2017-01-30 12:50:46 +0300

    8 задание, проблема с цветом и отображением графика

--- js/stat.js
+++ js/stat.js
@@ -12,7 +12,7 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов: ', 120, 60)
+  ctx.fillText('Список результатов: ', 120, 60);
 
   var max = -1;
 
@@ -21,7 +21,7 @@ window.renderStatistics = function (ctx, names, times) {
     if (time > max) {
       max = time;
     }
-
+  }
 
   var histoHeight = 150;
   var histoX = 140;
@@ -32,22 +32,37 @@ window.renderStatistics = function (ctx, names, times) {
     var name = names[i];
     var time = times[i];
 
-    var height = step * (time-min);
+    var height = step * time;
 
-    ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 90 + height;
-
-    if (name === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1');
+  if (name === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'rgba(255, 0, 0,', ((Math.random()*5)*5).toFixed(0);
+      ctx.fillStyle = 'rgba(0, 0, (parseInt(Math.random() * 5) * 10).toFixed(0))';
     }
 
+
     ctx.fillRect(histoX + columnIndent * i, 100, 40, height);
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = '#000000';
     ctx.fillText(name + ':' + time.toFixed(0), histoX + columnIndent * i, 100 + histoHeight + 20);
   }
 };
 
 var canvas = document.querySelector('canvas');
-window.renderStatistics(canvas.getContext('2d'), ['Катя', 'Лена', 'Вы'], [22.22, 33.33, 44.44]);
+renderStatistics(canvas.getContext('2d'), ['Иван', 'Игнат', 'Вы'], [22.22, 44.44, 33.33]);
 
+/*
+    var height = step * (time-min);
+
+    ctx.fillText(time.toFixed(0), histoX + columnIndent * i, 90 + height;
+
+    if (name === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, )', ((Math.random()*5)*5).toFixed(0);
+    }
+
+
+parseInt - округление числа до целого
+
+
+*/
